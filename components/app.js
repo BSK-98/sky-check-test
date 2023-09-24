ceba65685baf73e979dee2ef0d686ae3a4186b1a
BSK-98 <nathaniellee20graham@gmail.com>
2023-09-24 04:25:11 -0700

    added a nav bar and icons

--- components/app.js
+++ components/app.js
@@ -14,14 +14,44 @@ export const startWebcam  = (video) => {
 export const app = () => {
 
     return `
+        <div class="overlay"></div>
         <header>
             <div class="logo">
-                <h1>Skycheck</h1>
+                <h1>
+                    Skycheck 
+                    <span class="material-symbols-outlined">
+                        done
+                    </span>
+                </h1>
             </div>
         </header> 
+        <nav>
+            <div class="nav">
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        group
+                    </span>
+                </a>
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        widgets
+                    </span>
+                </a>
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        manage_accounts
+                    </span>
+                </a>
+                    <span class="material-symbols-outlined">
+                        home_app_logo
+                    </span>
+                </a>
+            </div>
+        </nav>
         <div class="video">
             <video id="video-previewer" autoplay></video>
         </div>
+        
     `
 }
 
